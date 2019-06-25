76484c828ba0238f5d2bc4473b482c0b89fc1c8c
温宇彤 <t@xtzero.me>
2019-06-25 14:41:34 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # xtgGthubWeebhookCi.php
 使用php配合github webhook进行自动部署
+
+需要安装php运行环境，php版本要求 > 5.6。
+
+在网站根目录部署`ci.php`，让该文件能被ip访问到。
+
+前往repo首页，依次选择`Settings-Webhooks-Add webhook`。在`Payload URL`中填入上述可执行的php的地址，比如`http://127.0.0.1/ci.php`。
+
+添加成功后尝试对repo进行一次push操作，前往服务器查看是否已经自动拉取成功了。
+
+php接收到push事件后会自动前往`web_root/repo_name`并执行`git pull`，相关代码在脚本的第28行。可以根据需要来修改。
---
+++ php/ci.php
@@ -0,0 +1,33 @@
+<?php
+function doShell ($cmd, $cwd = null) {
+    $descriptorspec = array(
+        0 => array("pipe", "r"),
+        1 => array("pipe", "w"),
+        2 => array("pipe", "w"),
+    );
+    $proc = proc_open($cmd, $descriptorspec, $pipes, $cwd, null);
+    if ($proc == false) {
+        return false;
+    } else {
+        $stdout = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
+        $status = proc_close($proc);
+    }
+    $data = array(
+        'stdout' => $stdout,
+        'stderr' => $stderr,
+        'retval' => $status,
+    );
+
+    return $data;
+}
+$postArr = json_decode($_POST['payload'],true);
+$repoName = $postArr['repository']['name'];
+$shell = 'cd /var/www/html/'.$repoName.' && sudo git pull';
+$res = doShell($shell);
+$resText = json_encode($res);
+file_put_contents("ci.log", $resText, FILE_APPEND);
+echo $resText;
+die();
